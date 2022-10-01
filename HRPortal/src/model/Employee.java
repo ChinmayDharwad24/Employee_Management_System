a067d7297aa00f977778de3b357456afecb3f63d
Chinmay Dharwad <dharwad.c@northeastern.com>
2022-10-01 19:34:15 -0400

    View and Delete funtions added

---
+++ HRPortal/src/model/Employee.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author chinm
+ */
+public class Employee {
+    
+    private String name;
+    private String empid;
+    
+    
+    
+}
